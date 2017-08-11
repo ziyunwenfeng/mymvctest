908929e3fdaea73bb4136370ad79ed66afa3e133
ziyunwenfeng <1060327272@qq.com>
2017-08-11 14:16:48 +0800

    commit

---
+++ ssh/src/main/java/dao/UserDAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import entity.User;
+
+public class UserDAO {
+	private SessionFactory sessionFactory;
+	public void setSessionFactory(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	public List<User> getAllUser(){
+		String hql = "from user";
+		Session session = sessionFactory.getCurrentSession();
+		Query query = session.createQuery(hql);
+		return (List<User>)query.list();
+	}
+}
---
+++ ssh/src/main/java/entity/User.java
@@ -0,0 +1,25 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity(name="t_user")
+public class User {
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="id")
+    private Integer id;    
+    @Column(name="name",length=32)
+    private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
